bd945ebf7b24223aec8ada603da640b08ca331f0
Daisuke Sato <tiryoh@gmail.com>
2024-08-05 12:17:19 +0900

    chore: Update install script

--- dist/install.sh
+++ dist/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# minimum
 sudo apt-get install ./mozc-utils-gui_2.28.4715.102+dfsg-2.2\~bpo22.04.1_amd64.deb ./mozc-server_2.28.4715.102+dfsg-2.2\~bpo22.04.1_amd64.deb ./mozc-data_2.28.4715.102+dfsg-2.2\~bpo22.04.1_all.deb ./ibus-mozc_2.28.4715.102+dfsg-2.2\~bpo22.04.1_amd64.deb ./fcitx5-mozc_2.28.4715.102+dfsg-2.2\~bpo22.04.1_amd64.deb ./fcitx-mozc-data_2.28.4715.102+dfsg-2.2\~bpo22.04.1_all.deb
+
+# install modules (include xorg)
+sudo apt-get install ./fcitx5-module-emoji_5.0.14-1_amd64.deb ./fcitx5-module-quickphrase_5.0.14-1_amd64.deb ./fcitx5-modules_5.0.14-1_amd64.deb ./fcitx5-modules-dev_5.0.14-1_amd64.deb ./fcitx5-module-wayland_5.0.14-1_amd64.deb ./fcitx5-module-xorg_5.0.14-1_amd64.deb
+
